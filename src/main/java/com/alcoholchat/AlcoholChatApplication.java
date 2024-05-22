a9bd890385aae0fb3a5a3caf11b9d863ceadabd9
inwoo <jiw413@naver.com>
2024-05-22 15:31:43 +0900

    init : project setting

---
+++ src/main/java/com/alcoholchat/AlcoholChatApplication.java
@@ -0,0 +1,13 @@
+package com.alcoholchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcoholChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcoholChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alcoholchat/AlcoholChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alcoholchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcoholChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
