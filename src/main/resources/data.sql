e69e7f6d7040077fa60aa7aa8e1b71a5f51b7219
inwoo <jiw413@naver.com>
2024-05-27 20:13:40 +0900

    feat : data.sql pub data 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
--- 데이터베이스 테이블 생성
+
 CREATE TABLE IF NOT EXISTS member (
     member_id BINARY(16) NOT NULL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
@@ -13,11 +13,24 @@ CREATE TABLE IF NOT EXISTS member (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
--- 인덱스 생성
-CREATE INDEX IF NOT EXISTS idx_role ON member (role);
+CREATE TABLE IF NOT EXISTS pub (
+    pub_id BINARY(16) NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    address VARCHAR(100) NOT NULL,
+    description VARCHAR(150) NOT NULL,
+    phone_num VARCHAR(11) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
 
--- 데이터 삽입 예시
+
+CREATE INDEX IF NOT EXISTS idx_role ON member (role);
 INSERT INTO member (member_id, email, password, nickname, phone_num, prefer_local, birth, role, is_deleted, created_at, updated_at)
 VALUES (X'123E4567E89B12D3A456426614174000', 'user1@example.com', 'password1', 'nickname1', '01012345678', 'Seoul', '1990-01-01', 'MEMBER', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        (X'223E4567E89B12D3A456426614174000', 'user2@example.com', 'password2', 'nickname2', '01023456789', 'Busan', '1992-02-02', 'ADMIN', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        (X'323E4567E89B12D3A456426614174000', 'user3@example.com', 'password3', 'nickname3', '01034567890', 'Incheon', '1994-03-03', 'MEMBER', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO pub (pub_id, name, address, description, phone_num, created_at, updated_at)
+VALUES (X'123E4567E89B12D3A456426614174000', 'Pub1', '123 Main St', 'A nice pub', '01012345678', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (X'223E4567E89B12D3A456426614174000', 'Pub2', '456 Side St', 'A cozy pub', '01023456789', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (X'323E4567E89B12D3A456426614174000', 'Pub3', '789 High St', 'A trendy pub', '01034567890', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
