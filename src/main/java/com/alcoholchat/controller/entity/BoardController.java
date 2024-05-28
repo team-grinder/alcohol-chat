1867474730b8e342daf00f5e14d429c2c33bf297
inwoo <jiw413@naver.com>
2024-05-28 13:56:07 +0900

    feat : controller base url

--- src/main/java/com/alcoholchat/controller/entity/BoardController.java
+++ src/main/java/com/alcoholchat/controller/entity/BoardController.java
@@ -3,11 +3,13 @@ package com.alcoholchat.controller.entity;
 import com.alcoholchat.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/board")
 public class BoardController {
     private final BoardService boardService;
 }
