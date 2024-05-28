3ac9da263c0c8c6a30aa99a5b9e76d8c86969f1c
inwoo <jiw413@naver.com>
2024-05-28 11:58:29 +0900

    feat : entity controller setting

---
+++ src/main/java/com/alcoholchat/controller/entity/BoardController.java
@@ -0,0 +1,13 @@
+package com.alcoholchat.controller.entity;
+
+import com.alcoholchat.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+}
---
+++ src/main/java/com/alcoholchat/controller/entity/CommentController.java
@@ -0,0 +1,13 @@
+package com.alcoholchat.controller.entity;
+
+import com.alcoholchat.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+}
