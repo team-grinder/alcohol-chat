6a81da77bd2768cf7560458c402f2bfa30d0adf7
inwoo <jiw413@naver.com>
2024-05-31 13:40:07 +0900

    feat : @ActiveProfiles("test") 추가

--- src/test/java/com/alcoholchat/service/PubServiceTest.java
+++ src/test/java/com/alcoholchat/service/PubServiceTest.java
@@ -3,9 +3,11 @@ package com.alcoholchat.service;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("test")
 class PubServiceTest {
 
     @BeforeEach
